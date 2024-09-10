c4cf499f6cdc2a5d1858b45d452a0cf7f1e1125b
Hritik003 <hritik.raj@nutanix.com>
2024-09-10 11:32:32 +0530

    fixed

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -3,36 +3,19 @@ package middleware
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"golang-react-todo-1/models"
 	"log"
 	"net/http"
 	"os"
 
-	"golang-react-todo-1/models"
-
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv" // Used for retrieving environment variables from .env file
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-// func EnableCors(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		// Set CORS headers to allow requests from the frontend
-// 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")            // Allow the frontend origin
-// 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE") // Allowed HTTP methods
-// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")     // Allow Content-Type header
-
-// 		if r.Method == "OPTIONS" {
-// 			w.WriteHeader(http.StatusOK)
-// 			return
-// 		}
-
-// 		// Pass the request down the chain
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
 
 var collection *mongo.Collection
 
@@ -44,7 +27,7 @@ func init() {
 func loadTheEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Fatal("Error loading the .env file")
 	}
 }
 
@@ -54,30 +37,28 @@ func createDBInstance() {
 	collName := os.Getenv("DB_COLLECTION_NAME")
 
 	clientOptions := options.Client().ApplyURI(connectionString)
-	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
 	if err != nil {
-		log.Fatalf("Error connecting to MongoDB: %v", err)
+		log.Fatal(err)
 	}
 
-	err = client.Ping(context.Background(), nil)
+	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Fatalf("Error pinging MongoDB: %v", err)
+		log.Fatal(err)
 	}
 
-	log.Println("Connected to MongoDB")
+	fmt.Println("connected to MongoDB!")
+
 	collection = client.Database(dbName).Collection(collName)
-	log.Printf("Collection instance created")
+	fmt.Println("collection instance created")
 }
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	payload, err := getAllTasks()
-	if err != nil {
-		http.Error(w, "Failed to return all tasks", http.StatusInternalServerError)
-		return
-	}
+	payload := getAllTasks()
 	json.NewEncoder(w).Encode(payload)
 }
 
@@ -86,13 +67,8 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	var task models.ToDo
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		http.Error(w, "Invalid task data", http.StatusBadRequest)
-		return
-	}
-
+	json.NewDecoder(r.Body).Decode(&task)
 	insertOneTask(task)
 	json.NewEncoder(w).Encode(task)
 }
@@ -104,13 +80,8 @@ func TaskComplete(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	params := mux.Vars(r)
-	id := params["id"]
-	if err := taskComplete(id); err != nil {
-		http.Error(w, "Failed to complete task", http.StatusInternalServerError)
-		return
-	}
-
-	json.NewEncoder(w).Encode(id)
+	taskComplete(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
 }
 
 func UndoTask(w http.ResponseWriter, r *http.Request) {
@@ -120,10 +91,7 @@ func UndoTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	params := mux.Vars(r)
-	if err := undoTask(params["id"]); err != nil {
-		http.Error(w, "Failed to undo task", http.StatusInternalServerError)
-		return
-	}
+	undoTask(params["id"])
 	json.NewEncoder(w).Encode(params["id"])
 }
 
@@ -134,95 +102,86 @@ func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	params := mux.Vars(r)
-	if err := deleteOneTask(params["id"]); err != nil {
-		http.Error(w, "Failed to delete task", http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
+	deleteOneTask(params["id"])
 }
 
 func DeleteAllTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	count, err := deleteAllTasks()
-	if err != nil {
-		http.Error(w, "Failed to delete all tasks", http.StatusInternalServerError)
-		return
-	}
+	count := deleteAllTasks()
 	json.NewEncoder(w).Encode(count)
+
 }
 
-func taskComplete(task string) error {
-	id, err := primitive.ObjectIDFromHex(task)
-	if err != nil {
-		return err
-	}
-	filter := bson.M{"_id": id}
-	update := bson.M{"$set": bson.M{"status": true}}
-	_, err = collection.UpdateOne(context.Background(), filter, update)
-	return err
-}
-
-func getAllTasks() ([]primitive.M, error) {
-	curr, err := collection.Find(context.Background(), bson.M{})
-	if err != nil {
-		log.Printf("Error fetching tasks from mongoDb: %v", err)
-		return nil, err
-	}
-
-	var results []primitive.M
-	for curr.Next(context.Background()) {
-		var result bson.M
-		if err := curr.Decode(&result); err != nil {
-			log.Printf("Error decoding task: %v", err)
-			return nil, err
-		}
-		results = append(results, result)
-	}
-
-	if err := curr.Err(); err != nil {
-		return nil, err
-	}
-
-	curr.Close(context.Background())
-	log.Printf("Fetched %d tasks", len(results))
-	return results, nil
-}
-
-func insertOneTask(task models.ToDo) {
-	_, err := collection.InsertOne(context.Background(), task)
+func getAllTasks() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Inserted task")
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		results = append(results, result)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cur.Close(context.Background())
+	return results
 }
 
-func undoTask(task string) error {
-	id, err := primitive.ObjectIDFromHex(task)
+func taskComplete(task string) {
+	id, _ := primitive.ObjectIDFromHex(task)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"status": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
+	fmt.Println("modified count:", result.ModifiedCount)
+}
+
+func insertOneTask(task models.ToDo) {
+	insertResult, err := collection.InsertOne(context.Background(), bson.M{"task": task})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Insert a single record", insertResult.InsertedID)
+}
+
+func undoTask(task string) {
+	id, _ := primitive.ObjectIDFromHex(task)
 	filter := bson.M{"_id": id}
 	update := bson.M{"$set": bson.M{"status": false}}
-	_, err = collection.UpdateOne(context.Background(), filter, update)
-	return err
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("modified count:", result.ModifiedCount)
 }
 
-func deleteOneTask(task string) error {
-	id, err := primitive.ObjectIDFromHex(task)
-	if err != nil {
-		return err
-	}
+func deleteOneTask(task string) {
+	id, _ := primitive.ObjectIDFromHex(task)
 	filter := bson.M{"_id": id}
-	_, err = collection.DeleteOne(context.Background(), filter)
-	return err
+	d, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Deleted Document", d.DeletedCount)
 }
 
-func deleteAllTasks() (int64, error) {
-	result, err := collection.DeleteMany(context.Background(), bson.M{})
+func deleteAllTasks() int64 {
+	d, err := collection.DeleteMany(context.Background(), bson.D{{}})
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
-	return result.DeletedCount, nil
+	fmt.Println("Deleted Document", d.DeletedCount)
+	return d.DeletedCount
 }
